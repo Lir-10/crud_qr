070e8bc59f9efb2f3bdf4c4fb8e44216dfa919e3
Lir-10 <uridx.gtrejo.9@gmail.com>
2022-10-05 19:46:32 -0500

    make it better

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('dashboard/users', compact('users'));
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('dashboard/create_user');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // Generar QR
+
+        \QrCode::size(100)
+                ->format('svg')
+                ->generate($user->email, public_path('qrs\qr_' . str_replace(' ', '_', $user->name)  . '.svg'));
+
+        // Actualizamos ruta de qR
+        $user->qr = 'qr_' . str_replace(' ', '_', $user->name)  . '.svg';
+        $user->save();
+
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        return view('dashboard/edit_user', compact('user'));
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+
+
+    public function update(Request $request, $id)
+    {
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email
+        ];
+
+        User::where('id', $id)->update($data);
+
+        $user = User::find($id);
+
+        if ($user->qr == null) {
+            \QrCode::size(100)
+                ->format('svg')
+                ->generate($user->email, public_path('qrs\qr_' . str_replace(' ', '_', $user->name)  . '.svg'));
+
+            // Actualizamos ruta de qR
+            $user->qr = 'qr_' . str_replace(' ', '_', $user->name)  . '.svg';
+            $user->save();
+        }
+
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id)->delete();
+        return redirect()->route('user.index')
+        ->with('Completado', 'Usuario eliminado correctamente');
+    }
+
+    /**
+     * Obtener la ruta del QR del usuario.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function showqr($id){
+        $user = User::find($id);
+
+        return response()->json(['route' => $user->qr], 200);
+    }
+
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,25 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Bienvenido <br>
+                    <p class="decoration-solid text-xl"> {{ Auth::user()->name }}</p>
+
+                    <div class="visible-print text-center">
+                        {!! QrCode::size(100)->generate
+                            ("Usuario: ".Request::user()->name." 
+"."Correo: ".Request::user()->email); !!}
+                        <p>Datos del Usuario en QR.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
